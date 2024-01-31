fb748307ff114437baebe3512f2813e50ab9e6e1
Raman Shekhawat <ramanshekhawat75@gmail.com>
2024-01-31 18:46:53 +0530

    push image

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -3,9 +3,9 @@ import mysql from "mysql2/promise";
 import * as schema from "./schema";
 import config from "../config";
 
-export const connection = await mysql.createConnection({
+export const connection = mysql.createPool({
   uri: config.dbUrl,
-  multipleStatements: true,
+  multipleStatements: true
 });
 
 export const db = drizzle(connection, { schema, mode: "default" });
--- backend/db/migrate.ts
+++ backend/db/migrate.ts
@@ -1,5 +1,16 @@
 import { migrate } from "drizzle-orm/mysql2/migrator";
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+import * as schema from "./schema";
+import config from "../config";
 
-import { db, connection } from "./index";
+export const connection = await mysql.createConnection({
+  uri: config.dbUrl,
+  multipleStatements: true
+});
+
+export const db = drizzle(connection, { schema, mode: "default" });
 
 await migrate(db, { migrationsFolder: "./backend/db/drizzle" });
+
+await connection.end();
