e1bf9e4efd756c101521c87b5d62b856d668b2c0
Raman Shekhawat <ramanshekhawat75@gmail.com>
2024-05-27 20:13:24 +0530

    db migrate for v1.3

--- backend/db/drizzle/0001_brown_champions.sql
+++ backend/db/drizzle/0001_brown_champions.sql
@@ -1,5 +1,7 @@
-DROP TABLE `roles`;--> statement-breakpoint
-ALTER TABLE `collection_roles` ADD `role_id` varchar(20) NOT NULL;--> statement-breakpoint
+ALTER TABLE `collection_roles` ADD  `role_id` varchar(20) NOT NULL;--> statement-breakpoint
 ALTER TABLE `token_roles` ADD `role_id` varchar(20) NOT NULL;--> statement-breakpoint
 ALTER TABLE `collection_roles` DROP PRIMARY KEY, ADD PRIMARY KEY(`server_id`,`collection_id`,`token_count`);--> statement-breakpoint
-ALTER TABLE `token_roles` DROP PRIMARY KEY, ADD PRIMARY KEY(`server_id`,`token_id`,`balance`);
+ALTER TABLE `token_roles` DROP PRIMARY KEY, ADD PRIMARY KEY(`server_id`,`token_id`,`balance`);--> statement-breakpoint
+UPDATE `token_roles` tr LEFT JOIN `roles` r ON tr.server_id = r.server_id AND tr.token_id = r.token_id SET tr.role_id = r.role_id;--> statement-breakpoint
+UPDATE `collection_roles` cr LEFT JOIN `roles` r ON cr.server_id = r.server_id AND cr.collection_id = r.collection_id SET cr.role_id = r.role_id;--> statement-breakpoint
+DROP TABLE `roles`;
