5c060b0ea899885c3d7bfed64faa1464f0051dc5
Raman Shekhawat <ramanshekhawat75@gmail.com>
2024-10-23 16:25:49 +0530

    Add cli to sync all roles

--- backend/cli/sync-roles.ts
+++ backend/cli/sync-roles.ts
@@ -1,29 +1,23 @@
-import { CronJob } from 'cron';
-
 import { db, schema } from '@/db';
 import { eq } from 'drizzle-orm';
 import { client } from '@/bot';
-import manageUserRoles from './util/manager';
+import manageUserRoles from '../util/manager';
 
-async function handle() {
+async function syncAllRoles() {
+    console.log('Syncing all roles...');
     const servers = await db.select().from(schema.servers);
     for (const server of servers) {
         const connectedAccounts = await db
             .select()
             .from(schema.connectedAccounts)
             .where(eq(schema.connectedAccounts.serverId, server.id));
+
         for (const account of connectedAccounts) {
             await manageUserRoles(client, server.id, account.userId);
         }
     }
+
+    console.log('All roles are synced!');
 }
 
-// This is a fail safe, in ideal case, the rpc event will evaluate accounts in real time.
-CronJob.from({
-    cronTime: '0 0 * * *', // every day at midnight
-    onTick: function () {
-        handle();
-    },
-    utcOffset: 0,
-    start: true
-});
+syncAllRoles();
