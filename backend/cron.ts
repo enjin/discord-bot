5d8935fb674a3559a3f8d6e304f4babda73fdb07
Raman Shekhawat <ramanshekhawat75@gmail.com>
2024-10-30 14:59:42 +0530

    feat: ENJ-205: Add CF headers to Indexer request & check indexer height (#10)

--- backend/cron.ts
+++ backend/cron.ts
@@ -1,29 +1,51 @@
-import { CronJob } from 'cron';
+import { CronJob } from "cron";
+import { expect } from "bun:test";
+import { db, schema } from "@/db";
+import { eq } from "drizzle-orm";
+import { client } from "@/bot";
+import config from "./config";
+import { getRpcApi } from "@/util/rpc-client";
+import { makeIndexerRequest } from "@/util/api";
+import manageUserRoles from "./util/manager";
 
-import { db, schema } from '@/db';
-import { eq } from 'drizzle-orm';
-import { client } from '@/bot';
-import manageUserRoles from './util/manager';
+async function checkIndexerHeight() {
+	const api = await getRpcApi();
+	const res = await makeIndexerRequest(
+		JSON.stringify({
+			query: `query {
+             squidStatus {
+                height
+            }
+        }`
+		})
+	);
+	const json = (await res.json()) as { data: { squidStatus: { height: number } } };
+	const indexerHeight = json.data.squidStatus.height;
+	const height = await api.query.system.number();
+	console.log(`Indexer height: ${indexerHeight}, Chain height: ${height.toPrimitive()}`);
+	expect(Math.abs(indexerHeight - (height.toPrimitive() as unknown as number))).toBeLessThan(10);
+}
 
 async function handle() {
-    const servers = await db.select().from(schema.servers);
-    for (const server of servers) {
-        const connectedAccounts = await db
-            .select()
-            .from(schema.connectedAccounts)
-            .where(eq(schema.connectedAccounts.serverId, server.id));
-        for (const account of connectedAccounts) {
-            await manageUserRoles(client, server.id, account.userId);
-        }
-    }
+	if (config.indexerUrl) {
+		await checkIndexerHeight();
+	}
+
+	const servers = await db.select().from(schema.servers);
+	for (const server of servers) {
+		const connectedAccounts = await db.select().from(schema.connectedAccounts).where(eq(schema.connectedAccounts.serverId, server.id));
+		for (const account of connectedAccounts) {
+			await manageUserRoles(client, server.id, account.userId);
+		}
+	}
 }
 
 // This is a fail safe, in ideal case, the rpc event will evaluate accounts in real time.
 CronJob.from({
-    cronTime: process.env.CRON_TIME || '0 0 * * *', // every day at midnight
-    onTick: function () {
-        handle();
-    },
-    utcOffset: 0,
-    start: true
+	cronTime: "0 0 * * *", // every day at midnight
+	onTick: function () {
+		handle();
+	},
+	utcOffset: 0,
+	start: true
 });
